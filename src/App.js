37c4bf64c6ed4c58b4fe047c24c2f94b55e1a921
Lori Henderson <lorim95@gmail.com>
2020-09-25 20:43:30 -0400

    Added link to backend portion and people I worked with

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoe: []
+    }
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/shoe')
+      .then((res) => res.json())
+      .then((data) => this.setState({shoe: data}));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>It's a Shoe's World</h1>
+        <ul>
+          {this.state.shoe.map((s) => (
+            <div>
+            <li>ID: {s.id} </li>
+            <ul><p>Brand Name: {s.brand_name}</p></ul>
+            <ul><p>Manufacturer: {s.manufacturer}</p></ul>
+            <ul><p>Size: {s.size}</p></ul>
+            <ul><p>Color: {s.color}</p></ul>
+            <ul><p>Material: {s.material}</p></ul>
+            <ul><p>Shoe Type: {s.shoe_type}</p></ul>
+            <ul><p>Fasten Type: {s.fasten_type}</p></ul>
+            <hr></hr>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+};
 
 export default App;
